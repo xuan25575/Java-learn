c8c8c5d51072a07872d1117f68466081fba4894f
25170 <xuanyu.huang@hand-china.com>
2019-08-22 16:58:24 +0800

    java 8 stream demo

---
+++ src/cn/ccu/stream/Demo05.java
@@ -0,0 +1,8 @@
+package cn.ccu.stream;
+
+/**
+ * @Description TODO
+ * @date 2019/8/22
+ */
+public class Demo05 {
+}
---
+++ src/cn/ccu/stream/test/Transaction.java
@@ -0,0 +1,8 @@
+package cn.ccu.stream.test;
+
+/**
+ * @Description TODO
+ * @date 2019/8/22
+ */
+public class Transaction {
+}
